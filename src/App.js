4df4529fb8b650425eb00b15de35149c7db9134f
dashaa <cybordashaa@gmail.com>
2020-04-12 20:19:42 +0800

    add(): function component button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const MyCoolButton = () =>  <input type="button" value="Click me!"/>;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MyCoolButton/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
